6d9dfe006d965a6a13a1b53bb5723b115e02c73e
Bernardo Santiago <bernardosantiago44@gmail.com>
2024-05-09 16:26:54 -0600

    Initial commit (created new project!)

---
+++ super-tic-tac-toe/super-tic-tac-toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  super-tic-tac-toe
+//
+//  Created by Bernardo Santiago Marin on 09/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ super-tic-tac-toe/super-tic-tac-toe/super_tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  super_tic_tac_toeApp.swift
+//  super-tic-tac-toe
+//
+//  Created by Bernardo Santiago Marin on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct super_tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
